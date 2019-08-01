ab94f672d18b85e7bce941f4d12091aeb3f575b6
EC2 Default User <ec2-user@ip-172-31-24-63.us-east-2.compute.internal>
2019-08-01 13:24:42 +0000

    day three

---
+++ app/models/model.py
@@ -0,0 +1,61 @@
+questions=[
+
+{"number":1,   
+"question" :"Is it legal for a man in Scotalnd to marry his widow's sister?",
+"answer" :"No",
+"points":10},
+    
+{"number":2,
+"question" :"A bat and a ball cost $1.10 in total. The bat costs $1 more than the ball. How much does the ball cost?",
+"answer" :"5 Cents",
+"points": 15},
+
+{"number":3,
+"question":"Forest is to tree as tree is to?",
+"answer": "Leaf",
+"points" :10},
+ 
+ {"number":4,
+ "question": "Do they have a Fourth of July in England?",
+ "answer" :"Yes",
+ "points": 10},
+ 
+{"number":5,
+"question":"You are participating in a race. You’ve just passed the second person. What position are you in?",
+"answer" : "2nd",
+"points" : 10},
+
+{"number" :6,
+"question": "Two people can make 2 bicycles in 2 hours. How many people are needed to make 12 bicycles in 6 hours?",
+"answer" : "4",
+"points":10},
+
+{"number": 7,
+"question":"Jack is taller than Peter, and Bill is shorter than Jack. Which of the following statements would be most accurate?",
+"answer":"It is impossible to tell whether Bill or Peter is taller",
+"points":15},
+
+{"number":8,
+"question":"Bob’s father has 4 children. Momo, Meme, and Mumu are three of them. Who’s the fourth?",
+"answer": "Bob",
+"points": 10},
+
+{"number":9, 
+"question": "Divide 30 by 1/2 and add 10.What is the answer?",
+"answer":70,
+"points":10},
+
+{"number":10,
+"question":"Some months have 31 days, how many have 28?",
+"answer":12,
+"points":10}
+
+
+
+
+
+
+
+
+
+]
---
+++ app/routes.py
@@ -0,0 +1,99 @@
+from app import app
+from flask import render_template, request
+from app.models import model, formopener
+# from model import questions
+
+@app.route('/')
+@app.route('/index')
+def index():
+    
+    return render_template("index.html")
+
+@app.route('/questions')
+
+def template():
+    return render_template("template.html", questions = model.questions)
+    
+        # TODO: write code...
+@app.route('/results' , methods = ['GET' , 'POST'])
+def results():
+    score = 50
+    message=""
+    
+    userdata=dict(request.form)
+    print (userdata)
+    widow=userdata["widow"]
+    print (widow)
+    bat=userdata["bat"]
+    print (bat)
+    forest=userdata["forest"]
+    print (forest)
+    july=userdata["july"]
+    print (july)
+    race=userdata["race"]
+    print(race)
+    bicycles=userdata["bicycles"]
+    print(bicycles)
+    height=userdata["height"]
+    print(height)
+    family=userdata["family"]
+    print(family)
+    Math=userdata["Math"]
+    print(Math)
+    Days=userdata["Days"]
+    print(Days)
+    
+    if widow== "no":
+        score+=10
+        
+    if bat== "5 cents":
+        score+=15
+        
+    if forest== "Leaf":
+        score+= 10
+        
+    if july== "Yes":
+        score+=10
+    
+    if race=="2nd":
+        score+=10
+        
+    if bicycles=="4 people":
+        score+=10
+            
+    if height=="It is impossible to tell whether Bill or Peter is taller":
+        score+=15
+        
+    if family=="Bob":
+        score+=10
+        
+    if Math=="70":
+        score+=10
+        
+    if Days=="12":
+        score+=10
+    
+     
+    if score>= 130 :
+        message= "Extremely high!What a genious!Aren't you proud of yourself?"
+        
+    elif score >= 120:
+        message="very high! outstanding performance "
+        
+    elif score >= 100:
+        message="High average! You're very talented! ?"
+        
+    elif score >= 90:
+         message="Average!You're doing a good job"
+        
+    elif score >= 80:
+        message="Low average. "
+        
+    elif score >= 70:
+        message="very low"
+        
+    elif score >= 69 :
+        message="Extremely low. "
+    
+    return render_template('results.html', score=score, message=message)
+    
